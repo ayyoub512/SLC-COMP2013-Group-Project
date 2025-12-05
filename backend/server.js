aafaed4c1ac2a9b36566326225fb42d519901edf
Ayyoub <ayyoub@musekits.com>
2025-12-05 15:59:33 -0500

    Securing admin routes with verify token function

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,23 @@ mongoose
 		console.log(error);
 	});
 
+// Function used to verify if the passed in token is valid or not
+// https://medium.com/@diego.coder/protect-your-routes-in-node-js-with-jwt-and-express-authentication-75ab80acf43a
+function verifyToken(req, res, next) {
+	const header = req.header("Authorization") || "";
+	const token = header.split(" ")[1];
+	if (!token) {
+		return res.status(401).json({ message: "Token not provied" });
+	}
+	try {
+		const payload = jwt.verify(token, JWT_SECRET);
+		req.username = payload.username;
+		next();
+	} catch (error) {
+		return res.status(403).json({ message: "Token not valid" });
+	}
+}
+
 // It works
 server.get("/", (request, response) => {
 	response.send("LIVE!");
@@ -42,94 +59,110 @@ server.get("/products", async (request, response) => {
 });
 
 // Adding a new product
-server.post("/add-product", async (request, response) => {
-	const { productName, brand, image, price } = request.body;
-	const id = crypto.randomUUID();
-	const product = new Product({
-		productName,
-		brand,
-		price,
-		image,
-		id
-	});
-
+server.post("/add-product", verifyToken, async (request, response) => {
 	try {
-		await product.save().then((result) => response.status(201).send(`${productName} added\nwith id: ${id}`));
+		// Checking if it's the admin.. req.username is a thing because fo the verifyToken function
+		if (request?.username !== "admin") {
+			return response.status(403).end("Only admin user can perform this action");
+		}
+		const { productName, brand, image, price } = request.body;
+		const id = crypto.randomUUID();
+		const product = new Product({
+			productName,
+			brand,
+			price,
+			image,
+			id
+		});
+
+		await product.save();
+		response.status(201).send(`${productName} added\nwith id: ${id}`);
 	} catch (error) {
-		console.log(error.message);
+		console.log("Error: ", error.message);
+		response.status(500).end("Error while adding product");
 	}
 });
 
 // Deleting a product by given :id
-server.delete("/products/:id", async (request, response) => {
-	const { id } = request.params;
+server.delete("/products/:id", verifyToken, async (request, response) => {
 	try {
-		await Product.findByIdAndDelete(id).then((result) => {
-			console.log(result);
-			response.status(200).send(result);
-		});
+		// Checking if it's the admin.. req.username is a thing because fo the verifyToken function
+		if (request?.username !== "admin") {
+			return response.status(403).end("Only admin user can perform this action");
+		}
+
+		const { id } = request.params;
+		const result = await Product.findByIdAndDelete(id);
+		console.log(result);
+		response.status(200).send(result);
 	} catch (error) {
 		console.log(error.message);
+		response.status(500).end("Error while deleting product");
 	}
 });
 
 // Upadting product by ID
-server.patch("/products/:id", async (request, response) => {
-	const prodId = request.params.id;
-	const { productName, brand, image, price, id } = request.body;
-
+server.patch("/products/:id", verifyToken, async (request, response) => {
 	try {
+		// Checking if it's the admin.. req.username is a thing because fo the verifyToken function
+		if (request?.username !== "admin") {
+			return response.status(403).end("Only admin user can perform this action");
+		}
+
+		const prodId = request.params.id;
+		const { productName, brand, image, price, id } = request.body;
+
 		await Product.findByIdAndUpdate(prodId, {
 			productName,
 			brand,
 			image,
 			price,
 			id
-		}).then((result) => response.status(200).send(`${productName} edited\nwith id: ${prodId}`));
+		});
+		response.status(200).send(`${productName} edited\nwith id: ${prodId}`);
 	} catch (error) {
 		console.log(error.message);
+		response.status(500).end("Error while updating product");
 	}
 });
 
 // Creating a new user route
 server.post("/create-user", async (request, response) => {
 	const { username, password } = request.body;
-	const hashedPassword = await bcrypt.hash(password, 12);
-
-	const newUser = new User({
-		username,
-		password: hashedPassword
-	});
-
+	console.log(username, password);
 	try {
-		await newUser.save().then((result) => response.send(`${username} is created with success!`));
+		//Hashing a password need bcrypt and salt rounds as an int
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const newUser = new User({
+			username,
+			password: hashedPassword
+		});
+		await newUser.save();
+		response.send({ message: `Congrats! created username ${username}` });
 	} catch (error) {
-		response.send(`create-user error: ${error.message}`);
+		console.log(error);
+		response.status(500).send({ message: `The username is already taken. Please try another!` });
 	}
 });
 
+// Login route
 server.post("/login", async (request, response) => {
-	try {
-		const { username, password } = request.body;
+	const { username, password } = request.body;
 
+	try {
 		const user = await User.findOne({ username });
 		if (!user) {
-			return responses.tatus(400).send({ message: "Username doesn't exist" });
+			return response.status(404).send({ message: "Username doesnot exist" });
 		}
 
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch) {
-			return response.status(400).send({ message: "Bad username or password" });
+		const match = await bcrypt.compare(password, user.password);
+		if (!match) {
+			return response.status(403).send({ message: "Bad username or password" });
 		}
 
-		const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: "1h" });
-
-		return response.send({
-			message: "User authenticated",
-			token
-		});
+		const jwtToken = jwt.sign({ id: user._id, username }, JWT_SECRET);
+		return response.status(201).send({ message: "User Authenticated", token: jwtToken });
 	} catch (error) {
-		console.error(error);
-		return response.status(500).send({ message: "Server error" });
+		response.status(500).send({ message: error.message });
 	}
 });
